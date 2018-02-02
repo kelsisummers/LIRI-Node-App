d1af2911625813e5286f8e58a563639ded071cb9
Kelsi Summers <kelsilsummers@gmail.com>
2018-02-01 21:55:19 -0700

    twitter

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'bwrqjFOd6ybYqASxSaZlXRMDl',
+  consumer_secret: 'zeCMygiJICw85S3FQwT70K1qZAeeeiB0HoQZDpfvSJetsiFqzv',
+  access_token_key: '958872616709668864-axpQvVo9j6GQVe5HrEJWU5C7XpAhH0f',
+  access_token_secret: 'kWxhCOuCd2DJbDCCzdCsqA9sRRtc4tLvZLcWHGRi0UWSD',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,6 @@
+var twitterKeys = require("./keys.js");
+
+console.log(twitterKeys);
+
+
+var twitterQuery= 'https://api.twitter.com/1.1/search/tweets.json'
